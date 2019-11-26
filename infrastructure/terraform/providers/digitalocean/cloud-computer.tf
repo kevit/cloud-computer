97710f3d7f9bb3782d394e76578698391a9db5e6
kevit <root@kevit.info>
2019-11-26 18:16:10 +0100

    initial digitalocean terraform provider

---
+++ infrastructure/terraform/providers/digitalocean/cloud-computer.tf
@@ -0,0 +1,62 @@
+locals {
+  environment_name = "cloud-computer-${var.CLOUD_COMPUTER_HOST_ID}-${random_id.instance_id.hex}"
+  machine_type = "nano"
+}
+
+module "cloud-computer" {
+  source = "github.com/terraform-digitalocean-modules/terraform-digitalocean-droplet"
+
+  droplet_count = 1
+
+  droplet_name       = "${var.environment_name}"
+  droplet_size       = "${var.machine_type}"
+  monitoring         = true
+  private_networking = true
+  ipv6               = true
+  floating_ip        = true
+  block_storage_size = 5
+  tags               = ["${digitalocean_tag.ENV_example.id}", "${digitalocean_tag.ROLE_web.id}"]
+  user_data          = "${file("user-data.web")}"
+}
+
+  provisioner "remote-exec" {
+    connection {
+      agent = false
+      private_key = "${tls_private_key.cloud-computer.private_key_pem}"
+      type = "ssh"
+      user = "root"
+    }
+
+    inline = [
+      "# Set cloud computer environment",
+      "export CLOUD_COMPUTER_CLOUD_PROVIDER_CREDENTIALS='${file("${var.cloud_provider_credentials_path}")}'",
+      "export CLOUD_COMPUTER_DNS_EMAIL=${var.CLOUD_COMPUTER_DNS_EMAIL}",
+      "export CLOUD_COMPUTER_DNS_TOKEN=${var.CLOUD_COMPUTER_DNS_TOKEN}",
+      "export CLOUD_COMPUTER_DNS_ZONE=${var.CLOUD_COMPUTER_DNS_ZONE}",
+      "export CLOUD_COMPUTER_HOST_ID=${var.CLOUD_COMPUTER_HOST_ID}",
+      "export CLOUD_COMPUTER_IMAGE=${var.CLOUD_COMPUTER_IMAGE}",
+      "export CLOUD_COMPUTER_REPOSITORY=${var.CLOUD_COMPUTER_REPOSITORY}",
+      "export CLOUD_COMPUTER_YARN_JAEGER_TRACE=${var.CLOUD_COMPUTER_YARN_JAEGER_TRACE}",
+      "export GIT_COMMITTER_EMAIL=${var.GIT_COMMITTER_EMAIL}",
+      "export GIT_COMMITTER_NAME=${var.GIT_COMMITTER_NAME}",
+
+      "# Alias docker run with cloud computer environment",
+      "alias docker_run=\"docker run --env CLOUD_COMPUTER_DNS_EMAIL --env CLOUD_COMPUTER_DNS_TOKEN --env CLOUD_COMPUTER_DNS_ZONE --env CLOUD_COMPUTER_HOST_ID --env CLOUD_COMPUTER_CLOUD_PROVIDER_CREDENTIALS --env CLOUD_COMPUTER_YARN_JAEGER_TRACE --env DOCKER_HOST=localhost --env GIT_COMMITTER_EMAIL --env GIT_COMMITTER_NAME --interactive --rm --tty --volume CLOUD_COMPUTER_REPOSITORY:$CLOUD_COMPUTER_REPOSITORY --volume /var/run/docker.sock:/var/run/docker.sock --workdir $CLOUD_COMPUTER_REPOSITORY\"",
+      "alias docker_run_root=\"docker_run --user root $CLOUD_COMPUTER_IMAGE\"",
+      "alias docker_run_non-root=\"docker_run $CLOUD_COMPUTER_IMAGE\"",
+
+      "# Clone the cloud computer repository",
+      "docker_run_root git clone --branch ${var.CLOUD_COMPUTER_GIT_BRANCH} --quiet ${var.CLOUD_COMPUTER_GIT_URL} $CLOUD_COMPUTER_REPOSITORY",
+
+      "# Set ownership of the CLOUD_COMPUTER_REPOSITORY volume",
+      "docker_run_root chown -R 1000:1000 $CLOUD_COMPUTER_REPOSITORY",
+
+      "# Start the cloud computer",
+      "docker_run_non-root yarn --cwd infrastructure/cloud-computer start",
+    ]
+  }
+
+  scheduling {
+    on_host_maintenance = "TERMINATE"
+  }
+}
---
+++ infrastructure/terraform/providers/digitalocean/variables.tf
@@ -0,0 +1,78 @@
+variable "cloud_provider_credentials_path" {
+  description = "The path to the file containing credentials for accessing the cloud provider."
+  default = "/cloud-computer/credentials/cloud-provider.json"
+}
+
+variable "machine_type" {
+  description = "The machine type."
+  default = "custom-4-12288"
+}
+
+variable "CLOUD_COMPUTER_CLOUD_PROVIDER_PROJECT" {
+  description = "The cloud provider project to create the machine in."
+}
+
+variable "CLOUD_COMPUTER_DNS_EMAIL" {
+  description = "The email address of the dns account."
+}
+
+variable "CLOUD_COMPUTER_DNS_TOKEN" {
+  description = "The token to access the dns account."
+}
+
+variable "CLOUD_COMPUTER_DNS_ZONE" {
+  description = "The dns account zone to update."
+}
+
+variable "CLOUD_COMPUTER_GIT_BRANCH" {
+  description = "The current git branch."
+}
+
+variable "CLOUD_COMPUTER_GIT_URL" {
+  description = "The current git url."
+}
+
+variable "CLOUD_COMPUTER_HOST_ID" {
+  description = "The id of the machine that created the instance."
+}
+
+variable "CLOUD_COMPUTER_HOST_NAME" {
+  description = "The hostname of the machine that created the instance."
+}
+
+variable "CLOUD_COMPUTER_HOST_USER" {
+  description = "The username of the user that created the instance."
+}
+
+variable "CLOUD_COMPUTER_IMAGE" {
+  description = "The cloud computer docker image."
+}
+
+variable "CLOUD_COMPUTER_REPOSITORY" {
+  description = "The cloud computer repoisitory path."
+}
+
+variable "CLOUD_COMPUTER_REGION" {
+  description = "The region to create the cloud computer virtual machine in."
+  default = "australia-southeast1"
+}
+
+
+variable "CLOUD_COMPUTER_YARN_JAEGER_TRACE" {
+  description = "The current cloud computer jaeger trace id."
+  default = ""
+}
+
+variable "GIT_COMMITTER_EMAIL" {
+  description = "The current git user email."
+  default = ""
+}
+
+variable "GIT_COMMITTER_NAME" {
+  description = "The current git user name."
+  default = ""
+}
+
+output "ip" {
+  value = "${google_compute_address.cloud-computer.address}"
+}
